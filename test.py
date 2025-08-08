ed0166b3ab84ec1cd12949fd92b9ea902bff415e
charanoverse <sricharankolachalama@gmail.com>
2025-08-09 00:56:22 +0530

    new

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from pdf_utils.pdf_parser import extract_text_chunks
 from pdf_utils.faiss_indexer import build_faiss_index
 
-chunks = extract_text_chunks("data/pdf2.pdf")
+chunks = extract_text_chunks("data/test1.pdf")
 build_faiss_index(chunks)
 exit()
